fb96f1a3f485dc3efd288bc1fdb38ad7c6536acf
Matt Cheetham <matt@3sidedcube.com>
2014-03-03 14:32:37 +0000

    Strips down language controller

--- Thunder Basics/TSCLanguageController.m
+++ Thunder Basics/TSCLanguageController.m
@@ -8,6 +8,12 @@
 
 #import "TSCLanguageController.h"
 
+@interface TSCLanguageController ()
+
+@property (nonatomic, strong) NSDictionary *languageDictionary;
+
+@end
+
 @implementation TSCLanguageController
 
 static TSCLanguageController *sharedController = nil;
@@ -17,15 +23,13 @@ static TSCLanguageController *sharedController = nil;
     return sharedController;
 }
 
-- (id)initWithContentController:(TSCContentController *)contentController
+- (id)initWithDictionary:(NSDictionary *)dictionary
 {
     self = [super init];
     
     if (self) {
         
-        self.contentController = contentController;
-        
-        [self reloadLanguagePack];
+        self.languageDictionary = dictionary;
         
     }
     
@@ -34,115 +38,11 @@ static TSCLanguageController *sharedController = nil;
     return self;
 }
 
-- (void)reloadLanguagePack
-{
-    [self loadLanguageFile:[self languageFilePath]];
-}
-
-- (NSString *)languageFilePath
-{
-    // Getting the user locale
-    NSLocale *locale = [NSLocale currentLocale];
-    
-    NSString *localeString = [locale localeIdentifier];
-    
-    // Re-arranging it to match the language pack filename
-    NSArray *localeComponents = [[localeString lowercaseString] componentsSeparatedByString:@"_"];
-    
-    NSString *language;
-    NSString *country;
-    
-    if (localeComponents && localeComponents.count > 1) {
-        language = [localeComponents objectAtIndex:0];
-        country = [localeComponents objectAtIndex:1];
-    } else {
-        NSLog(@"Error getting locale components from %@", localeString);
-        return NO;
-    }
-    
-    self.currentLanguage = [NSString stringWithFormat:@"%@_%@", country, language];
-    
-    /*
-     * Load users preferred languages and iterate over each. checking all language packs for similarities and loading the closest match
-     */
-    NSArray *preferredLanguages = [NSLocale preferredLanguages];
-    
-    NSArray *availablePacks = [self.contentController filesInDirectory:@"languages"];
-    
-    NSString *englishFallbackPack = nil;
-    
-    for (NSString *languageCode in preferredLanguages) {
-        
-        for (NSString *pack in availablePacks) {
-            
-            if (!englishFallbackPack && [pack rangeOfString:@"en"].location != NSNotFound) {
-                englishFallbackPack = [pack stringByDeletingPathExtension];
-            }
-            
-            if ([pack rangeOfString:languageCode].location != NSNotFound) {
-                
-                // Found a pack that's similar. Use it.
-                self.currentLanguage = [pack stringByDeletingPathExtension];
-                return [self.contentController pathForResource:self.currentLanguage ofType:@"json" inDirectory:@"languages"];
-                
-            }
-            
-        }
-        
-    }
-    
-    if (availablePacks.count > 0) {
-        self.currentLanguage = [[availablePacks objectAtIndex:0] stringByDeletingPathExtension];
-        return [self.contentController pathForResource:self.currentLanguage ofType:@"json" inDirectory:@"languages"];
-    }
-    
-    // IF this point is reached there are no languages. FAIL.
-    
-    /* Last ditch attempt at loading a language is to fallback to the first english back possible.. */
-    self.currentLanguage = englishFallbackPack;
-    return [self.contentController pathForResource:self.currentLanguage ofType:@"json" inDirectory:@"languages"];
-
-}
-
-- (void)loadLanguageFile:(NSString *)filePath
-{
-    if (filePath) {
-        
-        NSLog(@"<ThunderStorm> [Languages] Loading language at path %@", filePath);
-        
-        NSError *languageError;
-        NSData *data = [NSData dataWithContentsOfFile:filePath options:NSDataReadingUncached error:&languageError];
-        
-        if (languageError || !data) {
-            
-            NSLog(@"<ThunderStorm> [Languages] No data for language pack");
-            return;
-            
-        }
-        
-        NSDictionary *languageDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-        
-        self.languageDictionary = languageDictionary;
-        
-    } else {
-        
-        NSLog(@"<ThunderStorm> [Languages] File path was null");
-        return;
-    }
-}
+#pragma mark - Lookup methods
 
 - (NSString *)stringForKey:(NSString *)key
 {
     return [self stringForKey:key withFallbackString:nil];
-}
-
-- (NSString *)stringForDictionary:(NSDictionary *)dictionary
-{
-    if (![dictionary isEqual:[NSNull null]]) {
-        return [self stringForKey:dictionary[@"content"] withFallbackString:nil];
-    }
-    
-    return nil;
 }
 
 - (NSString *)stringForKey:(NSString *)key withFallbackString:(NSString *)fallbackString
