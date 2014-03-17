48c1db9974b61b4826aceeeab1bbb66136fb595c
Phillip Caudell <phill@3sidedcube.com>
2014-03-17 11:12:56 +0000

    Restores TSCWebViewController.

--- Thunder Basics/TSCWebViewController.h
+++ Thunder Basics/TSCWebViewController.h
@@ -1,13 +1,23 @@
 //
 //  TSCWebViewController.h
-//  ThunderBasics
+//  ThunderStorm
 //
-//  Created by Phillip Caudell on 17/03/2014.
-//  Copyright (c) 2014 3 SIDED CUBE. All rights reserved.
+//  Created by Phillip Caudell on 27/09/2013.
+//  Copyright (c) 2013 3 SIDED CUBE. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface TSCWebViewController : UIViewController
+@interface TSCWebViewController : UIViewController <UIWebViewDelegate, UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) NSURL *url;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (nonatomic, strong) UIBarButtonItem *backButtonItem;
+@property (nonatomic, strong) UIBarButtonItem *forwardButtonItem;
+@property (nonatomic, strong) NSURL *pendingURL;
+@property (nonatomic, assign) BOOL automaticallySetTitle;
+
+- (id)initWithURL:(NSURL *)url;
 
 @end
