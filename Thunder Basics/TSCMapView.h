f17589046c5a281c70fb5561d8b3fde7fe0e16c6
Phillip Caudell <phill@3sidedcube.com>
2014-05-29 10:46:55 +0100

    Adds TSCMapView

---
+++ Thunder Basics/TSCMapView.h
@@ -0,0 +1,13 @@
+//
+//  TSCMapView.h
+//  ThunderBasics
+//
+//  Created by Phillip Caudell on 29/05/2014.
+//  Copyright (c) 2014 3 SIDED CUBE. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface TSCMapView : MKMapView
+
+@end
---
+++ Thunder Basics/TSCMapView.m
@@ -0,0 +1,13 @@
+//
+//  TSCMapView.m
+//  ThunderBasics
+//
+//  Created by Phillip Caudell on 29/05/2014.
+//  Copyright (c) 2014 3 SIDED CUBE. All rights reserved.
+//
+
+#import "TSCMapView.h"
+
+@implementation TSCMapView
+
+@end
