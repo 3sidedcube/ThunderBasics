2eea405038b77d890eec26f5f5317f0721b8d9db
Matthew Cheetham <matt@3sidedcube.com>
2015-03-16 09:36:46 +0000

    Adds UIImage category which allows getting colour for a given pixel in an image

---
+++ ThunderBasics/UIImage+TSCImage.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+TSCImage.h
+//  ThunderBasics
+//
+//  Created by Simon Mitchell on 21/01/2015.
+//  Copyright (c) 2015 3 SIDED CUBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (TSCImage)
+
+- (UIColor *)pixelColorForX:(NSInteger)x y:(NSInteger)y;
+
+@end
