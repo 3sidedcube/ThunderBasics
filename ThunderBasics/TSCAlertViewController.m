052aab046f8d8c87a31f099232b64ab239ccaf33
Matthew Cheetham <matt@3sidedcube.com>
2015-03-16 09:59:01 +0000

    Adds stability to TSCAlertViewController

--- ThunderBasics/TSCAlertViewController.m
+++ ThunderBasics/TSCAlertViewController.m
@@ -14,6 +14,8 @@
 @property (nonatomic, strong) id retainer;
 @property (nonatomic, strong) NSMutableArray *activeActions;
 @property (nonatomic, strong) UIActionSheet *actionSheet;
+@property (nonatomic, strong) NSArray *textHandlers;
+@property (nonatomic, strong, readwrite) NSArray *textFields;
 
 - (NSArray *)alertActionsWithStyle:(TSCAlertActionStyle)style;
 - (void)handleActionAtIndex:(NSInteger)index;
@@ -45,8 +47,21 @@ static TSCAlertViewController *sharedController = nil;
 {
     NSMutableArray *actions = [NSMutableArray arrayWithArray:self.actions];
     [actions addObject:action];
-    
     _actions = actions;
+}
+
+- (void)addTextFieldWithConfigurationHandler:(TSCAlertTextFieldConfigurationHandler)handler
+{
+    NSMutableArray *textHandlers = [NSMutableArray arrayWithArray:self.textHandlers];
+    
+    if (handler) {
+        [textHandlers addObject:handler];
+    } else {
+        [textHandlers addObject:^void(UITextField *textField){
+            
+        }];
+    }
+    _textHandlers = textHandlers;
 }
 
 - (void)showInView:(UIView *)view
@@ -62,7 +77,7 @@ static TSCAlertViewController *sharedController = nil;
             [activeActions addObject:destructiveAction];
         }
     }
-
+    
     if (self.preferredStyle == TSCAlertViewControllerStyleAlert) {
         
         if (cancelAction) {
@@ -84,9 +99,25 @@ static TSCAlertViewController *sharedController = nil;
     if (self.preferredStyle == TSCAlertViewControllerStyleAlert) {
         
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:self.title message:self.message delegate:self cancelButtonTitle:cancelAction.title otherButtonTitles:nil];
-
-        for (TSCAlertAction *action in defaultActions) {
+        
+        alertView.alertViewStyle = [self alertViewStyle];
+        
+        NSMutableArray *textFields = [NSMutableArray new];
+        if (alertView.alertViewStyle == UIAlertViewStylePlainTextInput || alertView.alertViewStyle == UIAlertViewStyleSecureTextInput || alertView.alertViewStyle == UIAlertViewStyleLoginAndPasswordInput) {
             
+            [textFields addObject:[alertView textFieldAtIndex:0]];
+            if (alertView.alertViewStyle == UIAlertViewStyleLoginAndPasswordInput) {
+                [textFields addObject:[alertView textFieldAtIndex:1]];
+            }
+        }
+        self.textFields = textFields;
+        for (TSCAlertTextFieldConfigurationHandler textFieldHandler in self.textHandlers) {
+            
+            UITextField *textField = self.textFields[[self.textHandlers indexOfObject:textFieldHandler]];
+            textFieldHandler(textField);
+        }
+        
+        for (TSCAlertAction *action in defaultActions) {
             [alertView addButtonWithTitle:action.title];
         }
         
@@ -115,6 +146,37 @@ static TSCAlertViewController *sharedController = nil;
     self.activeActions = activeActions;
     
     [self TSC_retain];
+}
+
+- (UIAlertViewStyle)alertViewStyle
+{
+    if (self.textHandlers.count == 0) {
+        return UIAlertViewStyleDefault;
+    }
+    
+    if (self.textHandlers.count == 1) {
+        
+        TSCAlertTextFieldConfigurationHandler handler = self.textHandlers[0];
+        UITextField *dummyTextField = [UITextField new];
+        handler(dummyTextField);
+        
+        if (dummyTextField.secureTextEntry) {
+            return UIAlertViewStyleSecureTextInput;
+        } else {
+            return UIAlertViewStylePlainTextInput;
+        }
+    }
+    
+    if (self.textHandlers.count == 2) {
+        return UIAlertViewStyleLoginAndPasswordInput;
+    }
+    
+    if (self.textHandlers.count > 2) {
+        return UIAlertViewStyleLoginAndPasswordInput;
+        NSLog(@"No more than two text fields are supported in a UIAlertView at any one time.");
+    }
+    
+    return UIAlertViewStyleDefault;
 }
 
 #pragma mark Helpers
