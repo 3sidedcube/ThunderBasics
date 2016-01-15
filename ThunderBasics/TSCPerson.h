e92ee82381fa66fd4ef8b6272a09a28d3b3502ec
Simon Mitchell <simon@3sidedcube.com>
2016-01-15 11:08:11 +0000

    Adds extension to UIButton to replace it's title label with an activity indicator

--- ThunderBasics/TSCPerson.h
+++ ThunderBasics/TSCPerson.h
@@ -21,97 +21,97 @@
  @param ref A `ABRecordRef` extracted from an `ABAddressBook`
  @return A populated TSCObject
  */
-- (instancetype)initWithABRecordRef:(ABRecordRef)ref;
+- (instancetype _Nonnull)initWithABRecordRef:(ABRecordRef _Nonnull)ref;
 
 /**
  Updates the current model using an `ABRecordRef`
  @param ref The record Ref to update the current person model with
  */
-- (void)updateWithABRecordRef:(ABRecordRef)ref;
+- (void)updateWithABRecordRef:(ABRecordRef _Nonnull)ref;
 
 /**
  Takes a CNContact from Contacts framework and converts it into a TSCPerson
  @param contact A CNContact object obtained using the Contacts framework
  */
-- (instancetype)initWithContact:(CNContact *)contact;
+- (instancetype _Nonnull)initWithContact:(CNContact  * _Nonnull )contact;
 
 /**
  Updates the current model using a `CNContact`
  @param contact The contact to update the current person model with
  */
-- (void)updateWithCNContact:(CNContact *)contact;
+- (void)updateWithCNContact:(CNContact * _Nonnull)contact;
 
 /**
  Updates the current model using a `TSCPerson`
  @param person The person to update the current person model with
  */
-- (void)updateWithPerson:(TSCPerson *)person;
+- (void)updateWithPerson:(TSCPerson * _Nonnull)person;
 
 /**
  The initials of the person generated from any available first and last names
  */
-- (NSString *)initials;
+- (NSString * _Nonnull)initials;
 
 /**
  Generates a placeholder image using the users initials
  @param initials An NSString of initials to put into the placeholder image
  @return An image consisting of a grey circle with overlaying initial text.
  */
-- (UIImage *)contactPlaceholderWithInitials:(NSString *)initials;
+- (UIImage * _Nonnull)contactPlaceholderWithInitials:(NSString * _Nonnull)initials;
 
 /**
  The persons first name, if one could be found
  */
-@property (nonatomic, copy) NSString *firstName;
+@property (nonatomic, copy) NSString * _Nullable firstName;
 
 /**
  The persons last name, if one could be found
  */
-@property (nonatomic, copy) NSString *lastName;
+@property (nonatomic, copy) NSString * _Nullable lastName;
 
 /**
  The persons full name
  @discussion Generated using the available `firstName` and `lastName` properties
  */
-@property (nonatomic, copy) NSString *fullName;
+@property (nonatomic, copy) NSString * _Nullable fullName;
 
 /**
  An array of numbers extracted from the addressbook for the person
  */
-@property (nonatomic, strong) NSArray *numbers;
+@property (nonatomic, strong) NSArray <NSString *> * _Nullable numbers;
 
 /**
  An email address for the person, if one could be found
  */
-@property (nonatomic, copy) NSString *email;
+@property (nonatomic, copy) NSString * _Nullable email;
 
 /**
  A photo of the person at thumbnail resolution
  */
-@property (nonatomic, strong) UIImage *photo;
+@property (nonatomic, strong) UIImage * _Nullable photo;
 
 /**
  A photo of the person at it's original resolution
  */
-@property (nonatomic, strong) UIImage *largeImage;
+@property (nonatomic, strong) UIImage * _Nullable largeImage;
 
 /**
  The record ID number of the record in relation to the addressbook it was extracted from
  @note This record ID could change and Apple recommends implementing checks to make sure you get the expected person back when using this ID.
  */
-@property (nonatomic, strong) NSNumber *recordNumber __attribute((deprecated("Please use recordIdentifier instead")));
+@property (nonatomic, strong) NSNumber * _Nullable recordNumber __attribute((deprecated("Please use recordIdentifier instead")));
 
 /**
  The record ID of the record in relation to where it was extracted from
  @note Befors iOS 9 this record ID could change and Apple recommends implementing checks to make sure you get the expected person back when using this ID.
  */
-@property (nonatomic, strong) id recordIdentifier;
+@property (nonatomic, strong) id _Nonnull recordIdentifier;
 
 /**
  The persons mobile number, if available.
  @discussion This is populated using any number set to mobile or iPhone in the contact record
  */
-@property (nonatomic, copy) NSString *mobileNumber;
+@property (nonatomic, copy) NSString * _Nullable mobileNumber;
 
 /**
  If the user has no photo set, a placeholder will be generated and this boolean will be set to true
@@ -122,6 +122,6 @@
 /**
  The observer object that listens for changes in the address book
  */
-@property (nonatomic, strong) id <NSObject> observer;
+@property (nonatomic, strong) id <NSObject> _Nullable observer;
 
 @end
