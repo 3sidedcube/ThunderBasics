50e35c8ba3431eb5c3a73f69d8b1f9a2bb28e1dd
Simon Mitchell <simon@3sidedcube.com>
2017-08-14 10:59:32 +0100

    Adds navigation helpers for popping easily to a view controller of a particular class, or a view controller that isn't of a particular set of classes

---
+++ ThunderBasics/Navigation.swift
@@ -0,0 +1,49 @@
+//
+//  Navigation.swift
+//  GNAH
+//
+//  Created by Simon Mitchell on 14/08/2017.
+//  Copyright © 2017 3sidedcube. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+	
+	/// Pops to the most recent view controller in the stack which is of a particular class
+	///
+	/// - Parameters:
+	///   - viewControllerClass: The class of view controller to pop to
+	///   - animated: Whether to animate the transition
+	/// - Returns: A boolean as to whether a view controller of this class was popped to or not
+	func popToLastViewController(of viewControllerClass: AnyClass, animated: Bool = true) -> Bool {
+		
+		guard let previousViewController = navigationController?.viewControllers.filter({ (viewController) -> Bool in
+			return viewController.isMember(of: viewControllerClass)  && viewController != self
+		}).last else { return false }
+		
+		navigationController?.popToViewController(previousViewController, animated: animated)
+		return true
+	}
+	
+	/// Pops to the most recent view controller which isn't a member of any of the classes we don't want to pop to
+	///
+	/// - Parameters:
+	///   - classes: Classes of view controller to avoid popping to
+	///   - animated: Whether to animate the transition
+	/// - Returns: A boolean as to whether a valid view controller was found and popped to or not
+	func popToLastViewController(excluding classes: [AnyClass], animated: Bool = true) -> Bool {
+		
+		guard let previousViewController = navigationController?.viewControllers.filter({ (viewController) -> Bool in
+			// If there is a class which this view controller is a member of, then don't allow popping to it
+			let isOneOfClass = classes.first(where: { (vcClass) -> Bool in
+				return viewController.isMember(of: vcClass)
+			}) != nil
+			
+			return !isOneOfClass && viewController != self
+		}).last else { return false }
+		
+		navigationController?.popToViewController(previousViewController, animated: animated)
+		return true
+	}
+}
