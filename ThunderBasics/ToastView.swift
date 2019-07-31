dab792c467252605800c64f2af936bddc755107b
Ryan Bourne <ryan.bourne@3sidedcube.com>
2019-07-31 11:40:51 +0100

    Changed ToastView access modifiers to allow for usage

--- ThunderBasics/ToastView.swift
+++ ThunderBasics/ToastView.swift
@@ -43,7 +43,7 @@ public class ToastView: UIView {
     ///   - message: The message text to display on the toast
     ///   - image: The image to display on the left hand side of the toast
     ///   - action: The action to be called if the user taps the toast
-    init(title: String?, message: String?, image: UIImage?, action: ToastActionHandler?) {
+    public init(title: String?, message: String?, image: UIImage?, action: ToastActionHandler?) {
         
         super.init(frame: .zero)
         
@@ -89,7 +89,7 @@ public class ToastView: UIView {
     /// Shows the toast notification on the screen
     ///
     /// - Parameter completion: A completion block to be called when the toast notification has displayed and dismissed successfully
-    func show(completion: @escaping () -> Void) {
+    public func show(completion: @escaping () -> Void) {
         
         frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44) // Add on 40 points so when it drops down you can't see the view behind it.
         layout()
