7c8e82a4d6f18c8befdd1835a489b0ebec8f3ec2
Simon Mitchell <simon@3sidedcube.com>
2019-08-22 16:21:50 +0100

    Adds method via extension to `UIView` which can be used to calculate the size that a view will occupy when constrained to a certain width or height

---
+++ ThunderBasics/UIView+IntrinsicContentSize.swift
@@ -0,0 +1,74 @@
+//
+//  UIView+IntrinsicContentSize.swift
+//  ThunderBasics-iOS
+//
+//  Created by Simon Mitchell on 22/08/2019.
+//  Copyright © 2019 threesidedcube. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    /// Returns the intrinsic content size an auto-layout based view will occupy when constrained
+    /// to the given size and allowed to resize in the given axis.
+    ///
+    /// - Parameters:
+    ///   - constrainedTo: The size the view is allowed to occupy.
+    ///   - resizableAxis: The axis the view is allowed to resize in.
+    /// - Returns: The size the view will occupy
+    /// - Important: This will only behave nicely on views layed out using layout constraints as it essentialy adds a width or height constraint and calls `layoutSubviews()`. It must also therefore be run on the main thread!
+    /// - Note: The returned size's width/height can be returned as smaller than the relative `constrainedSize` property if the content fits easily within the constrained width.
+    func intrinsicContentSize(constrainedTo constrainedSize: CGSize, resizableAxis: NSLayoutConstraint.Axis = .vertical) -> CGSize {
+        
+        let size: CGSize
+        
+        // Keep track of these so we can re-set them after we're done calculating
+        let translates = translatesAutoresizingMaskIntoConstraints
+        let mask = autoresizingMask
+        translatesAutoresizingMaskIntoConstraints = true
+        
+        if resizableAxis == .vertical {
+            
+            let width = NSNumber(value: Float(constrainedSize.width))
+            let temporaryWidthConstraint = NSLayoutConstraint.constraints(withVisualFormat: "[view(width)]", options: NSLayoutConstraint.FormatOptions.init(rawValue: 0), metrics: ["width": width], views: ["view": self])
+            addConstraints(temporaryWidthConstraint)
+            bounds = CGRect(origin: .zero, size: CGSize(width: constrainedSize.width, height: 0))
+            
+            autoresizingMask = .flexibleHeight
+            setNeedsUpdateConstraints()
+            
+            // Force the view to layout itself and any children
+            setNeedsLayout()
+            updateConstraints()
+            layoutSubviews()
+            
+            size = systemLayoutSizeFitting(constrainedSize)
+            removeConstraints(temporaryWidthConstraint)
+            
+        } else {
+            
+            let height = NSNumber(value: Float(constrainedSize.height))
+            let temporaryHeightConstraint = NSLayoutConstraint.constraints(withVisualFormat: "[view(height)]", options: NSLayoutConstraint.FormatOptions.init(rawValue: 0), metrics: ["height": height], views: ["view": self])
+            addConstraints(temporaryHeightConstraint)
+            bounds = CGRect(origin: .zero, size: CGSize(width: constrainedSize.width, height: 0))
+            
+            autoresizingMask = .flexibleHeight
+            setNeedsUpdateConstraints()
+            
+            // Force the view to layout itself and any children
+            setNeedsLayout()
+            updateConstraints()
+            layoutSubviews()
+            
+            size = systemLayoutSizeFitting(constrainedSize)
+            removeConstraints(temporaryHeightConstraint)
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = translates
+        autoresizingMask = mask
+        
+        return size
+    }
+}
