f4e42516702a3e44cca4d036c2b324ca2cb2c3a6
Simon Mitchell <simon@3sidedcube.com>
2019-09-05 16:43:32 +0100

    Adds AccessibleButton `UIButton` subclass which allows for dynamic font sizes and multi-line title text

---
+++ ThunderBasics/AccessibleButton.swift
@@ -0,0 +1,45 @@
+//
+//  AccessibleButton.swift
+//  ThunderBasics-iOS
+//
+//  Created by Simon Mitchell on 05/09/2019.
+//  Copyright © 2019 threesidedcube. All rights reserved.
+//
+
+import UIKit
+
+/// A subclass of `TSCButton` which enables automatic font adjustments, and allows for multi-line text
+class AccessibleButton: TSCButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        titleLabel?.numberOfLines = 0
+        titleLabel?.lineBreakMode = .byWordWrapping
+        titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return titleLabel?.intrinsicContentSize ?? super.intrinsicContentSize
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return titleLabel?.sizeThatFits(size) ?? super.sizeThatFits(size)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let titleLabel = titleLabel {
+            titleLabel.preferredMaxLayoutWidth = titleLabel.frame.width
+        }
+        super.layoutSubviews()
+    }
+}
