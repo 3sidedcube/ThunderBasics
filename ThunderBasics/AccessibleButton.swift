312ebfd1a7901b88f44ca968d479f39322d81860
Simon Mitchell <simon@3sidedcube.com>
2019-09-06 12:11:57 +0100

    Undoes previous commit

--- ThunderBasics/AccessibleButton.swift
+++ ThunderBasics/AccessibleButton.swift
@@ -55,7 +55,6 @@ open class AccessibleButton: TSCButton {
     }
     
     override open func layoutSubviews() {
-        super.layoutSubviews()
         let contentWidth = bounds.width - contentEdgeInsets.left - contentEdgeInsets.right
         let imageWidth = imageView?.bounds.width ?? 0 + imageEdgeInsets.left + imageEdgeInsets.right
         let titleMaxWidth = contentWidth - imageWidth - titleEdgeInsets.left - titleEdgeInsets.right
