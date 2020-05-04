03214e81394ab9e63869c768a7c38492f3716df8
benshutt <ben@3sidedcube.com>
2020-05-04 17:09:25 +0100

    Use `TimeZone.current` in `DateFormatter+Extensions`, add `DateFormatterExtensionsTests`

--- ThunderBasics/DateFormatter+Extensions.swift
+++ ThunderBasics/DateFormatter+Extensions.swift
@@ -11,19 +11,35 @@ import Foundation
 public extension DateFormatter {
     
     /// Standard `iso8601` `DateFormatter`
-    /// Note milliseconds included, for customization over `dateFormat`, see `iso8601(dateFormat:timeZone:)`
-    static var iso8601: DateFormatter {
+    /// Note milliseconds included, for customization over `dateFormat`,
+    /// see `iso8601Formatter(dateFormat:timeZone:)`
+    static var iso8601Millis: DateFormatter {
         return iso8601Formatter(
             dateFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        )
+    }
+    
+    /// Standard `iso8601` `DateFormatter`
+    /// Note milliseconds included, for customization over `dateFormat`,
+    /// see `iso8601Formatter(dateFormat:timeZone:)`
+    static var iso8601: DateFormatter {
+        return iso8601Formatter(
+            dateFormat: "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         )
     }
     
     /// `DateFormatter` has:
     /// - `.iso8601` `Calendar`
     /// - "en_US_POSIX" `Locale`
-    /// - Given `timeZone`, defaults to GMT
+    /// - Given `timeZone`, defaults to `TimeZone.current`
     /// - Given `dateFormat`
-    static func iso8601Formatter(dateFormat: String, timeZone: TimeZone? = TimeZone(secondsFromGMT: 0)) -> DateFormatter {
+    /// - Parameters:
+    ///   - dateFormat: Given `String` date format
+    ///   - timeZone: Given `TimeZone`, defaults to `TimeZone.current`
+    static func iso8601Formatter(
+        dateFormat: String,
+        timeZone: TimeZone? = TimeZone.current
+    ) -> DateFormatter {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
