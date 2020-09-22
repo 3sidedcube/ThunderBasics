88ac5269714ad34c1904f8c8824ea73279c2dbc4
Simon Mitchell <simon@3sidedcube.com>
2020-09-22 12:17:48 +0100

    Updates migrate.sh script

--- IBMigrationTool/migrate.sh
+++ IBMigrationTool/migrate.sh
@@ -1,1 +1,12 @@
-sh IBMigrationTool 
+
+bin_path="IBMigrationTool"
+migrationtool_binary_download_url="https://raw.githubusercontent.com/3sidedcube/ThunderBasics/feature/migration_tool_improvements/IBMigrationTool/IBMigrationTool"
+
+echo " => Downloading IBMigrationTool from (${migrationtool_binary_download_url})
+
+curl -fL --progress-bar --output "IBMigrationTool" "$migrationtool_binary_download_url"
+
+echo " => Making it executable ..."
+chmod +x "${bin_path}"
+
+sh ${bin_path}
