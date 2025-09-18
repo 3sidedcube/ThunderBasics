27323ba82ff3ff184c48e8efa318af0b8d5ea5df
Sam Davis <samdavis@3sidedcube.com>
2025-09-18 14:53:06 +0100

    Remove ThunderBasics umbrella target and Swift file
    Deleted ThunderBasics/ThunderBasics.swift and updated Package.swift to remove the ThunderBasics umbrella target. The package now directly exposes ThunderBasicsSwift and ThunderBasicsObjC as separate targets, simplifying the package structure.

--- Package.swift
+++ Package.swift
@@ -9,37 +9,30 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ThunderBasics",
             targets: [
-                "ThunderBasics",
+                "ThunderBasics"
             ]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ThunderBasics",
             dependencies: [
-                .target(name: "ThunderBasicsSwift"),
                 .target(name: "ThunderBasicsObjC")
             ],
-            path: "ThunderBasics"
-        ),
-        .target(
-            name: "ThunderBasicsSwift",
             path: "ThunderBasicsSwift"
         ),
         .target(
             name: "ThunderBasicsObjC",
-            path: "ThunderBasicsObjC"
+            path: "ThunderBasicsObjC",
+            publicHeadersPath: "include"
         ),
         .testTarget(
             name: "ThunderBasicsTests",
-            dependencies: ["ThunderBasicsSwift"],
+            dependencies: ["ThunderBasics"],
             path: "ThunderBasicsTests"
-        ),
+        )
     ]
 )
--- ThunderBasics/ThunderBasics.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ThunderBasics.swift
-//  ThunderBasics
-//
-//  Created by Sam Davis on 24/01/2025.
-//
-
-@_exported import ThunderBasicsSwift
-@_exported import ThunderBasicsObjC
